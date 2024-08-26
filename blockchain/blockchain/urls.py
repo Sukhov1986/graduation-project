30d7d6ad0fb7dda6dec1311dc98f470d1288c891
Igor_Sukhov <mebunu94@gmail.com>
2024-08-26 09:41:36 +0300

    ckeditor

--- blockchain/blockchain/urls.py
+++ blockchain/blockchain/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('crypto/', include('crypto.urls')),
     path('captcha/', include('captcha.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blockchain/news/admin.py
+++ blockchain/news/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from .models import News, Comments
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from django import forms
+
+class NewsAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget())
+
+
 
 class NewsAdmin(admin.ModelAdmin):
+    form = NewsAdminForm
     list_display = ('title', 'published_date', 'total_likes')
     search_fields = ('title', 'content',)
     list_filter = ('published_date', )
